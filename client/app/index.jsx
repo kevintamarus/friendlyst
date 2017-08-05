91554eb597c2f4fbf1954c0584723e492721e9f7
Joseph Cheng <joecheng950103@gmail.com>
2017-08-04 17:32:32 -0700

    set up redux

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,1 +1,28 @@
-console.log('works')
+import { render } from 'react-dom'
+import React from 'react'
+import { createStore, combineReducers, applyMiddleware} from 'redux'
+import { Provider } from 'react-redux'
+import App from './App.jsx'
+
+
+const reducer = (state={}, action) => {
+    if (action.type = 'INC') {
+        state = Object.assign({}, state, {
+            num: state.num + action.payload
+        })
+        return state
+    }
+    return state
+}
+
+const store = createStore(reducer, {
+    num: 0
+})
+
+
+
+render(
+<Provider store={store}>
+    <App />
+</Provider>
+,document.getElementById('app'))
