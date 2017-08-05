754cdf7e47bd9eb23f0c6e408efce04c1581cec7
Joseph Cheng <joecheng950103@gmail.com>
2017-08-05 08:39:45 -0700

    made some minor changes

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -3,13 +3,21 @@ import { connect } from 'react-redux'
 
 
 class App extends Component {
-    constructor() {
-        super()
+
+    changeName() {
+        let name = document.getElementById('i').value 
+        document.getElementById('i').value = ''
+        this.props.name(name)
     }
+
     render() {
         return (
             <div>
-                {this.props.num}
+                {this.props.num.num}
+                {this.props.user.name}
+                <input type="text" id="i"/>
+                <button onClick={this.changeName.bind(this)}>X</button>
+                <button onClick={this.props.dispatch}>X</button>
             </div>
         )
     }
@@ -17,16 +25,29 @@ class App extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        num: state.num
+        num: state.math,
+        user: state.user
     }
 }
 
 const mapDispathToProps = (dispatch) => {
     return {
-        changeNum(num) {
-            dispath({
+        changeNum() {
+            dispatch({
                 type: 'INC',
-                payload: num
+                payload: 1
+            })
+        },
+        name(n) {
+            dispatch({
+                type: 'CHANGE_NAME',
+                payload: n
+            })
+        },
+        dec() {
+            dispatch({
+                type: 'DEC',
+                payload: 1
             })
         }
     }
--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,23 +1,46 @@
 import { render } from 'react-dom'
 import React from 'react'
 import { createStore, combineReducers, applyMiddleware} from 'redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
 
+const initialState = {
+    num: 0,
+    name: 'joe'
+}
 
-const reducer = (state={}, action) => {
-    if (action.type = 'INC') {
+const mathReducer = (state=initialState, action) => {
+    if (action.type === 'INC') {
         state = Object.assign({}, state, {
             num: state.num + action.payload
+        })
+        return state
+    } else if (action.type == 'DEC') {
+        state = Object.assign({}, state, {
+            num: state.num - action.payload
         })
         return state
     }
     return state
 }
 
-const store = createStore(reducer, {
-    num: 0
+const userReducer = (state=initialState, action) => {
+    if (action.type === 'CHANGE_NAME') {
+        state = Object.assign({}, state, {
+            name: action.payload
+        })
+        return state
+    }
+    return state
+}
+
+const reducers = combineReducers({
+    user: userReducer,
+    math: mathReducer
 })
+
+const store = createStore(reducers)
 
 
 
