766766296d4280b13082a54aab2934e85e87d89d
Joseph Cheng <joecheng950103@gmail.com>
2017-08-09 11:09:50 -0700

    added minimize and maximize functions

--- client/app/FriendList.jsx
+++ client/app/FriendList.jsx
@@ -8,17 +8,33 @@ class FriendList extends Component {
   componentDidMount() {
   }
 
+  minimize() {
+    $('.friend-list-container').hide()
+    $(".friend-list-container-minimize").show()
+  }
+
+  maximize() {
+    $('.friend-list-container').show()
+    $(".friend-list-container-minimize").hide()
+  }
+
   render() {
 
     return(
-      <div className="friend-list-container">
-        <p>Chat</p>
-        <div className="friend-list">
-          {
-            this.props.friends.map((friend) => {
-              return <FriendListEntry friend={friend} appendChatRoom={this.props.appendChatRoom} mainUser={this.props.mainUser}/>
-            })
-          }
+      <div>
+        <div className="friend-list-container">
+          <p onClick={this.minimize}>Chat</p>
+          <div className="friend-list">
+            {
+              this.props.friends.map((friend) => {
+                return <FriendListEntry friend={friend} appendChatRoom={this.props.appendChatRoom} mainUser={this.props.mainUser}/>
+              })
+            }
+          </div>
+        </div>
+
+        <div className="friend-list-container-minimize" style={{display:'none'}} onClick={this.maximize}>
+          <p onClick={this.maximize}>Chat</p>
         </div>
       </div>
     )
