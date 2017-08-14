6be76aecce2e2cabbd7f3bb76a3b0b2b0a09a50e
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-08-14 13:39:37 -0700

    posts updates in correct order now

--- client/app/FeedList.jsx
+++ client/app/FeedList.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import FeedListEntry from './FeedListEntry.jsx'
 
-const FeedList = ({ posts, previousPosts, user }) => {
+const FeedList = ({ posts, user }) => {
   return (
     <div>
       <div>
-        {posts.map((post, key) => <FeedListEntry post={post} key={post.id} user={user} />)}
-      </div>
-      <div>
-        {previousPosts.map((post, key) => <FeedListEntry post={post} key={post.id} user={user} />)}
+        {posts.sort( (a,b) => {
+				a = a.updatedAt;
+				b = b.updatedAt;
+				return a > b ? -1 : a < b ? 1 : 0;
+			}).map((post, key) => <FeedListEntry post={post} key={post.id} user={user} />)}
       </div>
     </div>
   )
